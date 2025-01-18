3b126026eae84ca86aa979036e28c0139c10139f
Benjamin Schoolland <bschoolland@gmail.com>
2025-01-18 13:09:39 -0800

    feat: section 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from '@/components/layout/Navbar'
 import { Hero } from '@/components/sections/Hero'
 import { UIUXDesign } from '@/components/sections/UIUXDesign'
+import { WebsiteDevelopment } from '@/components/sections/WebsiteDevelopment'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <UIUXDesign />
+      <WebsiteDevelopment />
     </>
   )
 }
