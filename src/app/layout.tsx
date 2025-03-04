c59a1364c21905b517df68fbca7e6edef78b1d0e
Benjamin Schoolland <bschoolland@gmail.com>
2025-03-03 17:05:36 -0800

    feat: amplitude

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Visions to Visuals | Trusted Websites for your Business",
@@ -13,6 +14,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="overflow-x-hidden bg-black">
+      <head>
+        <Script src="https://cdn.amplitude.com/libs/analytics-browser-2.11.1-min.js.gz" strategy="beforeInteractive" />
+        <Script src="https://cdn.amplitude.com/libs/plugin-session-replay-browser-1.8.0-min.js.gz" strategy="beforeInteractive" />
+        <Script id="amplitude-init" strategy="afterInteractive">
+          {`
+            window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));
+            window.amplitude.init('e4f679d7fba1f871c1dc78e0bee4ac47', {"autocapture":{"elementInteractions":true}});
+          `}
+        </Script>
+      </head>
       <body className="font-tt-hoves antialiased overflow-x-hidden">
         {children}
       </body>
